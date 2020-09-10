cd4b425b215c81e607a9f1bd7bb52e4450007fc7
Nievl <lzhnek@gmail.com>
2020-09-10 12:39:52 +0300

    added error handler

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -13,6 +15,22 @@ export class UserService {
     return this._http.get("/api/users");
   }
   public update(name: string, newName: string) {
-    return this._http.put(`/api/users/${name}`, { full_name: newName });
+    return this._http
+      .put(`/api/users/${name}`, { full_name: newName })
+      .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error("An error occurred:", error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    if (error.status === 404) {
+      return throwError("User not found");
+    }
+    return throwError("Something bad happened; please try again later.");
   }
 }
